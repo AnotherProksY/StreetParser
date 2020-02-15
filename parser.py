6a425fe846f6634e448876236768ced251ac0706
Kamil <k.fazilov@icloud.com>
2020-02-15 20:18:41 +0300

    Изменил нейминг

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 """Script for mapping streets and their districts."""
 from sys import argv
 
-DISTRICT_FILES = {
+DISTRICTS_FILES = {
     "ЦАО": "CAO.txt",
     "САО": "SAO.txt",
     "СВАО": "SVAO.txt",
@@ -14,7 +14,7 @@ DISTRICT_FILES = {
     "ЗЕЛАО": "ZELAO.txt"
 }
 
-STREET_LIST_FILE = f'{argv[1]}'
+STREET_LIST_FILE = str(argv[1])
 
 
 districts_count = {
@@ -40,7 +40,7 @@ def read(filename):
 
 def street_parser(style="Raw"):
     """
-    1. Iterate through DISTRICT_FILES dictionary.
+    1. Iterate through DISTRICTS_FILES dictionary.
 
        * Obtain "code : filename"
        * Assign return value from read() -> (list of file lines)
@@ -49,21 +49,21 @@ def street_parser(style="Raw"):
 
        * reassign value to street variable -> (list of [street_name, count])
 
-    3. Iterate through district_name -> (each line of DISTRICT_FILES.filename).
+    3. Iterate through district_streets -> (each line of DISTRICTS_FILES.filename).
 
        * if line contains streets.street.name,
             then append street.count value to districts_count dictionary
     """
     streets = read(STREET_LIST_FILE)
 
-    for code, filename in DISTRICT_FILES.items():
-        district_name = read(filename)
+    for code, filename in DISTRICTS_FILES.items():
+        district_streets = read(filename)
 
         for street in streets:
             street = street.split(',')
 
-            for line in district_name:
-                if street[0] in line:
+            for district_street in district_streets:
+                if street[0] in district_street:
                     districts_count[code] += int(street[1])
 
     # After iterating and parsing, print dictionary values
@@ -76,4 +76,4 @@ def street_parser(style="Raw"):
 
 
 if __name__ == "__main__":
-    street_parser(f'{argv[2]}')
+    street_parser(str(argv[2]))
